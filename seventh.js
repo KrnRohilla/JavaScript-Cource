f50a000667244767f94563579806e49c755a4e0a
karan <karanr2810btechcse@kccint.edu.in>
2024-12-19 15:33:03 +0530

    JavaScript-Learning

--- seventh.js
+++ seventh.js
@@ -43,3 +43,71 @@
 // divs[1].innerText = "Unique value Y";
 // divs[2].innerText = "Unique value Z";
 
+//* Attributes
+// Get
+// Example
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// Set
+// Example
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class","newClass"));
+
+// Style
+// Example
+// let div = document.querySelector("div");
+// div.style.backgroundColor ="blue";
+
+//* Insert Elements
+// Append-Example
+// let newBtn = document.querySelector("button");
+// console.log(newBtn);
+// let div = document.querySelector("div");
+// div.append(newBtn);
+
+// prepend-Example
+// let newBtn = document.querySelector("button");
+// console.log(newBtn);
+// let div = document.querySelector("div");
+// div.prepend(newBtn);
+
+// Before-Example
+// let newBtn = document.querySelector("button");
+// console.log(newBtn);
+// let div = document.querySelector("div");
+// div.before(newBtn);
+
+// After-Example
+// let newBtn = document.querySelector("button");
+// console.log(newBtn);
+// let div = document.querySelector("div");
+// div.after(newBtn);
+
+//* Delete Elements
+// Example
+// let para = document.querySelector("p");
+// para.remove();
+
+//* Practice-Question
+// Question-1
+// create a new button element. give it a text "Click me" , background color of red & text color of white
+// insert the button as the first element inside the body tag
+// let newBtn = document.createElement("button");
+// console.log(newBtn);
+// newBtn.textContent = "Click me";
+// newBtn.style.backgroundColor = "red";
+// newBtn.style.color ="white";
+// let body = document.querySelector("body");
+// body.prepend(newBtn);
+
+// Question-2
+// create a <p> tag in html, give it a class & some styling.
+// now create a new class in css and try to append this class to the <p> element.
+// Did you notice, how you overwrite the class name when you add a new one?
+// solve this problem using classList.
+// let para = document.querySelector("p");
+// para.classList.add("newClass");
